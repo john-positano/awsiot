7c844400b3c5b5ac78bfa7a701029b2590041d9e
John Positano <johnpositano@Administrators-MacBook-Pro.local>
2017-05-08 10:16:56 -0400

    abc

--- app/routes/push.js
+++ app/routes/push.js
@@ -1,7 +1,8 @@
 module.exports = router = (require('express')).Router();
 
 router.post('/', function(req, res, next) {
+	console.log(req.body);
 	req.app.wsServer.emit('buttonUpdate', req.body);
-	res.status(200).end();
+	res.status(200).send('a');
 	return;
 });
